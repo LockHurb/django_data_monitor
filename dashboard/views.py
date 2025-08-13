4b799d273ec7c89b6a6a8715c75cd374d94275bb
Annabella Sánchez <ansaguzm@espol.edu.ec>
2025-08-13 13:21:53 -0500

    Guia 23

---
+++ dashboard/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+import requests
+from django.conf import settings
+# Create your views here.
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def index(request):
+    response = requests.get(settings.API_URL)  # URL de la API
+    posts = response.json()  # Convertir la respuesta a JSON
+
+    # Número total de respuestas
+    total_responses = len(posts)
+    data = {
+        'title': "Landing Page Dashboard",
+        'total_responses': total_responses
+    }
+    return render(request, "dashboard/index.html", data)
